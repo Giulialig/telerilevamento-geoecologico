9cec3230c2cecad8407732c3a9531b12aaa93306
Giulialig <128491206+Giulialig@users.noreply.github.com>
2023-03-21 15:54:51 +0100

    Create codice.r

---
+++ codice.r
@@ -0,0 +1,11 @@
+#installare pacchetto raster
+library(raster)
+setwd("C:\Users\GIULIA\Desktop\Landsat 2011")
+getwd()
+#importare
+l2011 <- brick("p224r63_2011_masked.grd")
+
+
+#importre immgine
+l2011 <- brick("p224r63_2011_masked.grd")
+l2011<-brick("p224r63_2011_masked.grd")
